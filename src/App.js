4f99298ab20f8995290e6547557dea989a8d8ea5
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2023-09-06 23:08:11 +0300

    initialize leaflet map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {MapContainer, TileLayer} from "react-leaflet";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       Hello world!
+        <div id='map'>
+            <MapContainer center={[55.75, 37.61]} zoom={13}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
+
     </div>
   );
 }
--- src/store/routesSlice.js
+++ src/store/routesSlice.js
@@ -3,7 +3,7 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
     routes: {
-
+        route1: [55.75, 37.61]
     }
 }
 
@@ -11,10 +11,12 @@ const routesSlice = createSlice({
     name: 'routes',
     initialState: initialState,
     reducers: {
-
+        setRoute(state, action) {
+            state.push(action.payload)
+        }
     }
 })
 
-export const {} = routesSlice.actions;
+export const {setRoute} = routesSlice.actions;
 
 export default routesSlice.reducer;
